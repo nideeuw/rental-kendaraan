8b4a381f63043f3d0f36967b4acc3e84c3093002
niky tania <nikytania0701@gmail.com>
2025-11-24 08:41:08 +0700

    update

--- controllers/PelangganController.php
+++ controllers/PelangganController.php
@@ -49,7 +49,7 @@ class PelangganController
       $data = [
         'nama_pelanggan' => $_POST['nama_pelanggan'],
         'alamat'         => $_POST['alamat'],
-        'no_telepon'     => $_POST['no_telepon'], 
+        'no_telepon'     => $_POST['no_telepon'],
         'email'          => $_POST['email']
       ];
 
